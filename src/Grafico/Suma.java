3952beaefb3c3043cadbd075b022a8cd50abb4f6
JohannaCifuentes <cifuentes0903@gmail.com>
2018-10-01 09:50:55 -0500

    Entorno grafico

---
+++ src/Grafico/Suma.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grafico;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author cifuentes0903
+ */
+public class Suma {
+    
+        public static void main(String[] args) {
+
+    
+    String nombre,apellido,direccion;
+        int telefono, edad;
+        
+  JOptionPane.showMessageDialog(null, "Hello World");
+
+         
+       /* nombre= JOptionPane.showInputDialog("DIGITE SU NOMBRE");
+        apellido= JOptionPane.showInputDialog("DIGITE SU APELLIDO");
+        direccion= JOptionPane.showInputDialog("DIGITE SU DIRECCION");
+        telefono= Integer.parseInt(JOptionPane.showInputDialog(null, "DIGITE SU TELEFONO"));
+        edad= Integer.parseInt(JOptionPane.showInputDialog(null, "DIGITE SU EDAD"));
+        
+        
+        JOptionPane.showMessageDialog(null,"Su nombre es"+nombre+"\n"+"Su apellido es"+apellido+"\n"+"Su direccion es"+direccion+"\n"+"Su telefono es"+telefono+"\n"+"Su edad es"+edad);
+       //( JOptionPane.showMessageDialog(null,"Su apellido es"+apellido);
+       // JOptionPane.showMessageDialog(null,"Su direccion es"+direccion);
+        //JOptionPane.showMessageDialog(null,"Su telefono es"+telefono);
+        //JOptionPane.showMessageDialog(null,"Su edad es"+edad);*/
+        }
+}
