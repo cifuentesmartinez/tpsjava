ffa8f4e7dce70bd224301f230b4d028e57ace757
JohannaCifuentes <cifuentes0903@gmail.com>
2018-10-03 08:25:21 -0500

    CiclosModificados

--- src/Ciclos/For.java
+++ src/Ciclos/For.java
@@ -14,20 +14,36 @@ import javax.swing.JOptionPane;
 public class For {
     public static void main(String[] args) {
     
-    int canti,Num1,suma,cont;
+    int canti,cont,Num1,Contne,Conpos,Sumapo,Sumane,Promepo = 0,Promene=0;
+    cont= 0;
+    Sumapo=0;
+    Sumane=0;
     
-    suma = 0;
-    
+    Contne = 0;  
+    Conpos =0;
      canti= Integer.parseInt(JOptionPane.showInputDialog(null, "DIGITE LA CANTIDAD DE NUMEROS "));
      
       for(cont=0;cont<canti;cont++) {
-         Num1= Integer.parseInt(JOptionPane.showInputDialog(null, "DIGITE LA EDAD DE ALUMNOS "));
-         suma=suma+Num1;
+         Num1= Integer.parseInt(JOptionPane.showInputDialog(null, "DIGITE EL NUMERO "));
+         if (Num1>0){
+         Conpos++;
+         Sumapo=Sumapo+Num1;
+         Promepo=Sumapo/canti;
+         
+         }else{
+         Contne++;
+         Sumane=Sumane+Num1;
+         Promene=Sumane/canti;
+         }
          //cont++;
          
     }
-      JOptionPane.showMessageDialog(null,"la sumatoria de edades son: "+ suma);
-
+      JOptionPane.showMessageDialog(null,"la canti de positivos son: "+ Conpos);
+      JOptionPane.showMessageDialog(null,"la canti de negativos son: "+ Contne);
+      JOptionPane.showMessageDialog(null,"la sumatoria de positovs son: "+ Sumapo);
+      JOptionPane.showMessageDialog(null,"la sumatoria de negativos son: "+ Sumane);
+      JOptionPane.showMessageDialog(null,"la promedio de positivos son: "+ Promepo);
+      JOptionPane.showMessageDialog(null,"la promedio de positivos son: "+ Promene);
     
     }
     
